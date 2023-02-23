917979cdbe459b233be08f1704b0341d5678766b
Wim Van Roy <wim.vr@hotmail.com>
2023-02-23 14:09:30 +0100

    working alternative version

---
+++ examples/hysteresis_car_control/minlp/car_example_dsc.py
@@ -0,0 +1,189 @@
+"""Create MINLP for the hysteresis car problem."""
+
+import casadi as ca
+import numpy as np
+from crc_algo.opt.description import Description
+import matplotlib.pyplot as plt
+
+
+class DescriptionExt(Description):
+    """Extended description."""
+
+    def __init__(self):
+        Description.__init__(self)
+        self.M = 1e6
+        self.eps = 1e-9
+
+    def equal_if_on(self, trigger, equality, ns=1):
+        """Big M formulation."""
+        self.leq((-self.M*(1-trigger)) * np.ones((ns, 1)), equality)
+        self.leq(equality, self.M*(1-trigger) * np.ones((ns, 1)))
+
+    def higher_if_on(self, trigger, equation):
+        """Trigger = 1 if equation > -eps."""
+        self.leq(-(1-trigger) * self.M, equation + self.eps)
+        self.leq(equation + self.eps, self.M * trigger)
+
+    def geq(self, a, b):
+        return self.leq(b, a)
+
+
+# Parameters
+N_stages = 10
+N_control_intervals = 1
+N_finite_elements = 1
+time_as_parameter = False
+n_s = 4
+tau = ca.collocation_points(n_s, "radau")
+C_irk, D_irk, B_irk = ca.collocation_coeff(tau)
+
+# Hystheresis parameters
+psi_on = [10, 15]
+psi_off = [5, 10]
+
+# Model parameters
+q_goal = 150
+v_goal = 0
+v_max = 30
+u_max = 5
+M = 1e6
+
+# fuel costs:
+C = [1, 1.8, 2.5]
+# ratios
+n = [1, 2, 3]
+
+# State variables:
+q = ca.SX.sym("q")  # position
+v = ca.SX.sym("v")  # velocity
+L = ca.SX.sym("L")  # Fuel usage
+X = ca.vertcat(q, v, L)
+X0 = [0, 0, 0]
+lbx = [0, 0, -ca.inf]
+ubx = [ca.inf, v_max, ca.inf]
+q_goal = 100
+v_goal = 0
+n_x = 3
+# Binaries to represent the problem:
+n_y = 2  #len(n)
+Y0 = np.zeros(n_y)
+Y0[0] = 1
+u = ca.SX.sym('u')  # drive
+n_u = 1
+U0 = np.zeros(n_u)
+
+lbu = np.array([-u_max])
+ubu = np.array([u_max])
+
+# x = q v L
+X = ca.vertcat(q, v, L)
+
+F_dyn = [
+    ca.Function(f'f_dyn_{i}', [X, u], [ca.vertcat(
+        v, n[i]*u, C[i]
+    )]) for i in range(len(n))
+]
+
+psi = v
+psi_fun = ca.Function('psi', [X], [psi])
+
+
+# Create problem:
+opti = DescriptionExt()
+# Time optimal control
+if not time_as_parameter:
+    T_final = opti.sym("T_final", 1, lb=1e-2, ub=1e2, x0=15)
+else:
+    T_final = opti.add_parameters("T_final", 1, values=15)
+
+h = T_final/(N_stages*N_control_intervals*N_finite_elements)
+# Cost: time only
+J = T_final
+
+
+Xk = opti.sym("Xk", n_x, lb=X0, ub=X0, x0=X0)
+for k in range(N_stages):
+    Yk = opti.sym_bool("Yk", n_y)
+    if k == 0:
+        opti.eq(Yk[1], Y0[1])
+        opti.eq(Yk[0], Y0[0])
+    else:
+        eps = 0.01
+        opti.add_g(1 - eps, ca.sum1(Yk), 1 + eps)  # SOS1
+        # Transition condition
+        LknUp = opti.sym_bool("LknUp", n_y-1)
+        LknDown = opti.sym_bool("LknDown", n_y-1)
+        # Transition
+        LkUp = opti.sym_bool("LkUp", n_y-1)
+        LkDown = opti.sym_bool("LkDown", n_y-1)
+
+        psi = psi_fun(Xk)
+        for i in range(n_y-1):
+            # Trigger
+            # If psi > psi_on -> psi - psi_on >= 0 -> LknUp = 1
+            opti.higher_if_on(LknUp[i], psi - psi_on[i])
+            opti.higher_if_on(LknDown[i], psi_off[i] - psi)
+            # Only if trigger is ok, go up
+            opti.leq(LkUp[i], LknUp[i])
+            opti.leq(LkDown[i], LknDown[i])
+            # Only go up if active
+            opti.leq(LkUp[i], Yk[i])
+            opti.leq(LkDown[i], Yk[i+1])
+            # Force going up if trigger = 1 and in right state!
+            opti.leq(LknUp[i] + Yk[i] - 1, LkUp[i])
+            opti.leq(LknDown[i] + Yk[i + 1] - 1, LkDown[i])
+
+    for i in range(N_control_intervals):
+        Uk = opti.sym("U", n_u, lb=lbu, ub=ubu, x0=U0)
+
+        for j in range(N_finite_elements):
+            # n_s collocation points
+            Xc = ca.horzcat(*[
+                opti.sym("Xc", n_x, lb=lbx, ub=ubx, x0=X0)
+                for i in range(n_s)
+            ])
+
+            # Dynamics!
+            odes = [
+                Yk[i] * F_dyn[i](Xc, Uk)
+                for i in range(n_y)
+            ]
+
+            # Use collocation
+            Z = ca.horzcat(Xk, Xc)
+            Pidot = Z @ C_irk
+            for i in range(n_s):
+                eq = 0
+                for ii in range(n_y):
+                    eq += Yk[ii] * F_dyn[ii](Z[:, i], Uk)
+
+                opti.eq(Pidot[:, i], h * eq)
+
+            Xk_end = Z @ D_irk
+
+            # # J = J + L*B_irk*h;
+            # for ode_id, ode in enumerate(odes):
+            #     opti.equal_if_on(Yk[ode_id], Pidot[:] - h * ode[:], n_x*n_s)
+
+            Xk = opti.sym("Xk", n_x, lb=lbx, ub=ubx, x0=X0)
+            opti.eq(Xk, Xk_end)
+
+
+# Terminal constraints:
+slack1 = opti.sym('slack', 1, lb=0, ub=ca.inf)
+slack2 = opti.sym('slack', 1, lb=0, ub=ca.inf)
+opti.leq(Xk[0] - q_goal, slack1)
+opti.leq(q_goal - Xk[0], slack1)
+opti.leq(Xk[1] - v_goal, slack2)
+opti.leq(v_goal - Xk[1], slack2)
+J += Xk[2]
+opti.f = J + slack1 + slack2
+# opti.set_solver(ca.nlpsol, 'ipopt', is_discrete=False)
+opti.set_solver(ca.nlpsol, 'bonmin', is_discrete=True)
+opti.solve()
+print(opti.get("T_final"))
+Xk = np.array(opti.get("Xk"))
+plt.plot(Xk)
+plt.show()
+print(opti.get("Yk"))
+breakpoint()
